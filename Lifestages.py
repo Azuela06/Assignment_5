e8ac7cc18f3ad91ba22347226f84d1dc6e94f6d1
Azii <violetlovato05@gmail.com>
2021-11-18 08:55:40 +0800

    Tried doing the 3rd program

---
+++ Lifestages.py
@@ -0,0 +1,17 @@
+
+def Life():
+    human = int(input("How old are you?: "))
+    print(f"You are currently {human} years old")
+    if human <= 12:
+        print ("Hello, you are just a kid!")
+    elif 13 == human <=17:
+        print ("Welcome to your Teenage life.")
+    elif human == 18:
+        print("Congratulations on your Debut.")
+    else:
+        human >= 19
+        print("You are now an adult, be responsible!")
+
+print("Welcome! To know what stage you are in now... Please indicate how old are you")
+
+Life()
